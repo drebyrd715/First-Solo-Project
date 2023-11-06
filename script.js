223eb3d9c0a39f5139e69e78d48c5a117a23eef0
drebyrd715 <drebyrd715@gmail.com>
2023-11-06 14:46:37 -0500

    trying to set up speed

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 //////////////// DECLARE ALL MY CONST ////////////////////
-const gameBoard = document.querySelector("gameBoard");
+const gameBoard = document.querySelector("#gameBoard");
 ////// ctx is what we draw on ////////
 const ctx = gameBoard.getContext("2d") //game in 2d//
-const scoreBoard = document.querySelector("scoreBoard");
-const resetButton = document.querySelector("resetButton");
+const scoreBoard = document.querySelector("#scoreBoard");
+const resetButton = document.querySelector("#resetButton");
 //// need game width/height in variable so i can access them ////////
 const gameWidth = gameBoard.width;
 const gameHeight = gameBoard.height;
-////////////// select my colors ///////////////////
-
-
+/////// create game objects and select my colors for game in CSS //////
+let paddle1 = paddle1;
+let paddle2 = paddle2;
+let ball = ball;
+///////////// make ball size(I'll make it a const) /////////////
+const ballRadius = 10.5;
+///////// how far paddles moves after hitting button ////////////
+const paddleSpeed = 40;
+let intervalID; ////// used to repeat a certian function at given time-interval///////
+let ballSpeed = 1;
+/////////////////// Coordinates of ball /////////////////
