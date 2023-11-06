59a8547f48139e1c218a558f072e75170a3164c3
drebyrd715 <drebyrd715@gmail.com>
2023-11-06 14:20:07 -0500

    set width and height in variables

--- script.js
+++ script.js
@@ -4,5 +4,9 @@ const gameBoard = document.querySelector("gameBoard");
 const ctx = gameBoard.getContext("2d") //game in 2d//
 const scoreBoard = document.querySelector("scoreBoard");
 const resetButton = document.querySelector("resetButton");
+//// need game width/height in variable so i can access them ////////
+const gameWidth = gameBoard.width;
+const gameHeight = gameBoard.height;
+////////////// select my colors ///////////////////
 
 
