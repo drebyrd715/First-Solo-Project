e27877a5544d52c0dbb8628e3afdefd4317670a8
drebyrd715 <drebyrd715@gmail.com>
2023-11-06 14:15:43 -0500

    some variables

---
+++ script.js
@@ -0,0 +1,8 @@
+//////////////// DECLARE ALL MY CONST ////////////////////
+const gameBoard = document.querySelector("gameBoard");
+////// ctx is what we draw on ////////
+const ctx = gameBoard.getContext("2d") //game in 2d//
+const scoreBoard = document.querySelector("scoreBoard");
+const resetButton = document.querySelector("resetButton");
+
+
